f2b86caeca176ea610856e869453b85e52c2b827
Babatoonday Mattieu <32313099+bbmattieu9@users.noreply.github.com>
2020-08-26 15:38:39 +0100

    [Dashboard] Add data to dashboard
    Problem
     * Dashboard component does not have data
    
    Solution
     * Created class fields to hold data for the dashboard UI
    
    [Finishes]

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  Designation: string;
+  Username: string;
+  noOfTeamMemners: number;
+  totalCostOfAllProjects: number;
+  pendingTasks: number;
+  upComingProjects: number;
+  projectCost: number;
+
+  currentExpenditure: number;
+  availableFunds: number;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.Designation = 'Team Leader';
+    this.Username = 'Adeleke Adeyemi';
+    this.noOfTeamMemners = 100;
+    this.totalCostOfAllProjects = 300;
+    this.pendingTasks = 15;
+    this.upComingProjects = 2;
+    this.projectCost = 2113507;
+    this.currentExpenditure = 96788;
+    this.availableFunds = 52538;
   }
 
 }
