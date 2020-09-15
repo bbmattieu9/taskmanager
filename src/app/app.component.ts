3d34766c37f765454c53bd5534e53332a294b396
Babatoonday Mattieu <32313099+bbmattieu9@users.noreply.github.com>
2020-09-15 14:58:51 +0100

    [in-memory-data] Add in-memory-web-api
    Problem
     * App needs data source for CRUD operations
     * No models or interface for strong typing
     * No projects service for projects business logic and server communication
    
    Solution
     * Implemented angulars InMemoryWebApi to act as database for data manipulation
     * Added model for strong typing
     * Added projects-Srvice class to handle all projects data manipulation
    
    [Finishes]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as projects from '../api/projects/projects.json';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'taskManager';
+
+  projs: any = (projects as any).default;
+
+  ngOnInit(): void {
+      console.log('[JSON File]:', this.projs);
+
+  }
 }
---
+++ src/jotter.ts
@@ -0,0 +1,3 @@
+// !env.production ?
+
+// HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService) : []
