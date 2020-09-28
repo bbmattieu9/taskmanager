b5719d76187fbfd1d48be673f3f7ba37fdbd2103
Babatoonday Mattieu <32313099+bbmattieu9@users.noreply.github.com>
2020-09-28 23:30:03 +0100

    [Auth] Add login to projects
    Problem
     * No login functionality yet
    
    Solution
     * Added logic to log user in and out of projects
     * Added login service and model for server request and strong typing
    
    [Finishes]

--- src/app/shared/login.service.ts
+++ src/app/shared/login.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { LoginViewModel} from '../model/login-view-model';
+
+
+// import env
+import { environment } from '../../environments/environment';
+
+// Rxjs
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  private baseUrl = environment.baseUrl;
+  private loginUrl = 'api/authenticate';
+
+  constructor(private httpSrv: HttpClient) { }
+
+  currentUser: string = null;
+
+  // login
+  public login(userEntity: LoginViewModel): Observable<any> {
+    return this.httpSrv.post<any>(`${this.baseUrl}/${this.loginUrl}`, userEntity, {responseType: 'json'})
+    .pipe(map(user => {
+      if (user) {
+        this.currentUser = user.userName;
+      }
+      return user;
+    }));
+  }
+
+
+  // logout
+  public logout(): void {
+    this.currentUser = null;
+  }
 }
