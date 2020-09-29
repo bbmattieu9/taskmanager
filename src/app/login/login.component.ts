f776c98f7db62bd1fc63b17dffb0714be3cad889
Babatoonday Mattieu <32313099+bbmattieu9@users.noreply.github.com>
2020-09-29 13:48:37 +0100

    [Auth] Add login UI template
    Problem
     *  No UI template for login template file
    
    Solution
     * Created UI template form for login component
    
    [Finishes]

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
 
 import { LoginViewModel } from '../model/login-view-model';
 import { LoginService } from '../shared/login.service';
@@ -13,11 +15,49 @@ export class LoginComponent implements OnInit {
 
   loginModel: LoginViewModel = new LoginViewModel();
   loginError: '';
+  fieldTextType: boolean;
+  signInErrorMsg: any;
+  signinHasError: false;
+  userSignInForm: FormGroup;
+  emailFeedbackMsg: string;
+  isLoading = false;
 
   constructor(private router: Router,
-              private loginSrv: LoginService) { }
+              private loginSrv: LoginService,
+              private fb: FormBuilder) { }
 
-  ngOnInit(): void {
+              ngUnsubscribe: Subject<void> = new Subject<void>();
+
+
+  // Returns validation message based on rule e.g required rule
+  // Used when watching for changes in email input field
+  private validateMessage = {
+    required: 'Email is required',
+    email: 'Invalid Email'
+  };
+
+      /**
+       *  Sets feedback message for form control validation
+       *   @ param c: it means it retunrs the form control
+       *  In this case, the email form control
+       */
+      setFeedbackMessage(c: AbstractControl): void {
+        this.emailFeedbackMsg = '';
+        if ((c.touched || c.dirty) && c.errors) {
+          this.emailFeedbackMsg = Object.keys(c.errors).map(
+            key => this.emailFeedbackMsg += this.validateMessage[key]).join(' ');
+        }
+      }
+
+              ngOnInit(): void {
+                this.userSignInForm = this.fb.group({
+                  email: ['', [Validators.required, Validators.email]],
+                  password: ['', [Validators.required]]
+                });
+  }
+
+  toggleFieldTextType(): any {
+    this.fieldTextType = !this.fieldTextType;
   }
 
   onLogin(event): any {
